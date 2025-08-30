2ceeac65614d5e79ad8d353d382ff90e7d700f05
DropPredator <ionchiccc@gmail.com>
2025-08-30 19:29:19 +0300

    made changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,24 +4,29 @@ const allPopulationsQuery = document.querySelectorAll('.population');
 const averageElem = document.querySelector('.average-population');
 const totalElem = document.querySelector('.total-population');
 const populations = [];
+let length = allPopulationsQuery.length;
+let averagePopulation;
+const regex = /\s+|,|\./g;
 
 allPopulationsQuery.forEach((elem) => {
   populations.push(elem.innerText);
 });
 
 let totalPopulation = populations.reduce((sum, elem) => {
-  const changedElem = Number(elem.replaceAll(',', ''));
+  const changedElem = Number(elem.replaceAll(regex, ''));
 
   if (Number.isFinite(changedElem)) {
     return sum + changedElem;
   }
 
+  length -= 1;
+
   return sum;
 }, 0);
 
-let averagePopulation = Math.floor(
-  totalPopulation / allPopulationsQuery.length,
-);
+if (length > 0 && Number.isFinite(length)) {
+  averagePopulation = Math.floor(totalPopulation / length);
+}
 
 averagePopulation = new Intl.NumberFormat().format(averagePopulation);
 totalPopulation = new Intl.NumberFormat().format(totalPopulation);
